7868f5b74cb84d8b20e5f0ff7c8e02d28891e28a
zhongxia <418760129@qq.com>
2015-10-14 17:56:01 +0800

    init the project

---
+++ app/route.jsx
@@ -0,0 +1,21 @@
+"use strict";
+
+import { Router, Route } from 'react-router';
+import React, { PropTypes } from "react";
+import App  from './components/app';
+
+
+
+// Declarative route configuration (could also load this config lazily
+// instead, all you really need is a single root route, you don't need to
+// colocate the entire config).
+var routes = (
+    <Router>
+        <Route path="/" component={App}>
+        </Route>
+    </Router>
+);
+
+
+
+export default routes;
