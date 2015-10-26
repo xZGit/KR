025285cf0e8ad963a182232bbd0ebca456295cc4
zhongxia <418760129@qq.com>
2015-10-26 14:19:02 +0800

    add index page

---
+++ app/components/article.js
@@ -0,0 +1,39 @@
+/**
+ * Created by xx on 15/10/21.
+ */
+
+
+"use strict";
+
+import React, { PropTypes } from 'react';
+import AuthorBar from './AuthorBar';
+
+var Article = React.createClass({
+
+
+    article: React.PropTypes.shape({
+        id: React.PropTypes.string.isRequired,
+        author: React.PropTypes.string.isRequired,
+        headImg: React.PropTypes.string.isRequired,
+        title: React.PropTypes.string.isRequired,
+        create_time: React.PropTypes.string.isRequired,
+        introduce: React.PropTypes.string.isRequired
+    }).isRequired,
+
+    render() {
+
+        return (
+            <div className="article-meta">
+                <AuthorBar author={this.props.article.author} headImg={this.props.article.headImg} create_time={this.props.article.create_time} />
+                <div>
+                    <div><h1 className="title-h">{this.props.article.title}</h1></div>
+                    <div><h1 className="">{this.props.article.introduce}</h1></div>
+                </div>
+            </div>
+
+        );
+    }
+});
+
+
+export default Article;
